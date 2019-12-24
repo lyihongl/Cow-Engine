fbeed476d5296612da5958e98e7da23446e324c5
Yihong Liu <yihongliu00@gmail.com>
2019-12-23 23:26:20 -0500

    rectangle test

---
+++ Engine_CPP/Graphics/Shape/RectangleTest.hpp
@@ -0,0 +1,22 @@
+#include "Rectangle.hpp"
+#ifndef COW_RECTANGLETEST_HPP
+#define COW_RECTANGLETEST_HPP
+#define AddElement(x...) AddElement(4, x);
+
+namespace cow{
+    bool TestRectangle(){
+        cow::Rectangle r{}; 
+        r.AddElement(1.0, 2.0, 3.0, 4.0);
+        r.AddElement(5.0, 6.0, 7.0, 8.0);
+        auto a = r.GetData();
+        for(std::size_t i{0}; i < a.size(); i++){
+            if(a[i] != i+1){
+                //test failed
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+#endif
