d0f2e25f519a78ff8a1012e541e71a2dac87adfb
Yihong Liu <yihongliu00@gmail.com>
2020-02-12 16:37:35 -0500

    open gl render works

--- Engine_CPP/src/Main.cpp
+++ Engine_CPP/src/Main.cpp
@@ -2,44 +2,67 @@
 #include <iostream>
 #include "../inc/Engine.hpp"
 #include "../inc/GameData.hpp"
+
+#include <glad/glad.h>
+#define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
-#include <GL/gl.h>
+ 
+#include "../inc/linmath.h"
+ 
+#include <stdlib.h>
+#include <stdio.h>
 
-int main(int argc, char** argv)
-{
-    GLFWwindow* window;
-
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
-
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
+int main(int argc, char** argv){
+    if(!glfwInit()){
+        std::cout << "failed to initialize" << std::endl;
         return -1;
     }
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    /* Make the window's context current */
+    GLFWwindow* window;
+    window = glfwCreateWindow(1024, 768, "wow", nullptr, nullptr);
     glfwMakeContextCurrent(window);
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+    if(window == nullptr){
+        std::cout << "yo" << std::endl;
+    }
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
+    GLuint VertexArrayID;
+    glGenVertexArrays(1, &VertexArrayID);
+    glBindVertexArray(VertexArrayID);
+
+    static const GLfloat g_vertex_buffer_data[] = {
+        -1.0f, -1.0f, 0.0f,
+        1.0f, -1.0f, 0.0f,
+        0.0f, 1.0f, 0.0f,
+    };
+
+    GLuint vertexbuffer;
+    glGenBuffers(1, &vertexbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+    do{
         glClear(GL_COLOR_BUFFER_BIT);
 
-        /* Swap front and back buffers */
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDisableVertexAttribArray(0);
+
         glfwSwapBuffers(window);
-
-        /* Poll for and process events */
         glfwPollEvents();
-    }
-
-    glfwTerminate();
+        //std::cout<<"a"<<std::endl;
+    } while(glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0);
     return 0;
 }
+ 
 //int main(int argc, char** argv) {
 //    cow::StartGame();
 //    return 0;
