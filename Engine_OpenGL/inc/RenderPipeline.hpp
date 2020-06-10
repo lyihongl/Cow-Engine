a737549569f83ef1eebb70cdb8436c67b7185f38
Yihong Liu <yihongliu00@gmail.com>
2020-06-09 22:50:57 -0400

    semi opengl abstraction layer

--- Engine_OpenGL/inc/RenderPipeline.hpp
+++ Engine_OpenGL/inc/RenderPipeline.hpp
@@ -17,9 +17,10 @@
 #include <glm/gtx/string_cast.hpp>
 #include <memory>
 
-#include "../inc/Shaders.hpp"
-#include "../inc/linmath.h"
-#include "../inc/stb_image.h"
+#include "RenderUnit.hpp"
+#include "linmath.h"
+#include "shaders.hpp"
+#include "stb_image.h"
 
 namespace CowRender {
 
@@ -71,11 +72,44 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     std::cout << "window resized to (w, h): "
               << "( " << width << ", " << height << ") " << std::endl;
 }
-
-void DrawQuad() {
+void DrawQuad2(RenderUnit::RenderUnit &r) {
     float vertices[] = {
-        0.5f, 0.5f, 0.0f,    // top right
-        0.5f, -0.5f, 0.0f,   // bottom right
+        -0.1f, 0.5f, 0.0f,   // top right
+        -0.1f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f, 0.5f, 0.0f    // top left
+    };
+    //float vertices[] = {
+    //    0.5f, 0.5f, 0.0f,   // top right
+    //    0.5f, -0.5f, 0.0f,  // bottom right
+    //    0.0f, -0.5f, 0.0f,  // bottom left
+    //    0.0f, 0.5f, 0.0f    // top left
+    //};
+    unsigned int indices[] = {
+        // note that we start from 0!
+        0, 1, 3,  // first triangle
+        1, 2, 3   // second triangle
+    };
+    std::cout<<"DEBUG"<<__LINE__<<std::endl;
+    RenderUnit::RenderUnit ru;
+    RenderUnit::SetVertexData(ru, vertices, 12, 3);
+    RenderUnit::SetIndexData(ru, indices, 6);
+
+    glGenVertexArrays(1, &ru.VAO);
+    glBindVertexArray(ru.VAO);
+
+    //RenderUnit::InitVAO(ru);
+    RenderUnit::InitVBO(ru);
+    RenderUnit::InitEBO(ru);
+    glVertexAttribPointer(0, ru.Stride, GL_FLOAT, GL_FALSE, ru.Stride * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    r = ru;
+}
+
+void DrawQuad(int &v) {
+    float vertices[] = {
+        0.5f, 0.5f, 0.0f,   // top right
+        0.5f, -0.5f, 0.0f,  // bottom right
         0.0f, -0.5f, 0.0f,  // bottom left
         0.0f, 0.5f, 0.0f    // top left
     };
@@ -94,21 +128,29 @@ void DrawQuad() {
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    std::cout<<"DEBUG vbo size"<<sizeof(vertices)<<std::endl;
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    std::cout<<"DEBUG ebo size"<<sizeof(indices)<<std::endl;
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+
+    v = VAO;
 
     //glBindVertexArray(0);
 }
 
 void Draw(GLFWwindow* window, Shader& shader) {
-    std::cout<<"Draw"<<std::endl;
-    DrawQuad();
-    while (!glfwWindowShouldClose(window))
-    {
+    std::cout << "Draw" << std::endl;
+    //DrawQuad2();
+    RenderUnit::RenderUnit r;
+    int vao2;
+    DrawQuad2(r);
+    DrawQuad(vao2);
+    std::cout<<"DEBUG"<<__LINE__<<std::endl;
+    while (!glfwWindowShouldClose(window)) {
         // input
         // -----
         //processInput(window);
@@ -121,11 +163,14 @@ void Draw(GLFWwindow* window, Shader& shader) {
 
         // draw our first triangle
         //glUseProgram(shaderProgram);
-        //glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+        glBindVertexArray(r.VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
         //glDrawArrays(GL_TRIANGLES, 0, 6);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        // glBindVertexArray(0); // no need to unbind it every time 
- 
+        glBindVertexArray(vao2); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+        //glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        // glBindVertexArray(0); // no need to unbind it every time
+
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
@@ -134,7 +179,7 @@ void Draw(GLFWwindow* window, Shader& shader) {
 }
 
 void StartRender(GLFWwindow* window) {
-    std::cout<<"a"<<std::endl;
+    std::cout << "a" << std::endl;
     float vertices[] = {
         -0.5f, -0.5f, 0.0f,
         0.5f, -0.5f, 0.0f,
--- Engine_OpenGL/inc/RenderUnit.hpp
+++ Engine_OpenGL/inc/RenderUnit.hpp
@@ -22,39 +22,49 @@
 //#include "../inc/stb_image.h"
 
 namespace CowRender {
+namespace RenderUnit {
+
 struct RenderUnit {
-    std::unique_ptr<float> VertexData = nullptr;
-    std::unique_ptr<float> IndexData = nullptr;
-    std::size_t DataSize, Stride;
+    float* VertexData;
+    unsigned int* IndexData;
+    std::size_t VertexDataSize, IndexDataSize, Stride;
     unsigned int VBO, VAO, EBO;
 };
-void SetVertexData(RenderUnit& r, std::unique_ptr<float> data, std::size_t size, std::size_t stride){
-    r.VertexData = std::move(data);
-    r.DataSize = size;
+void SetVertexData(RenderUnit& r, float* data, std::size_t size, std::size_t stride) {
+    r.VertexData = data;
+    r.VertexDataSize = size;
     r.Stride = stride;
 }
 
-void InitVBO(RenderUnit& r){
-    if(r.VertexData!=nullptr){
+void SetIndexData(RenderUnit& r, unsigned int * data, std::size_t size) {
+    r.IndexData = data;
+    r.IndexDataSize = size;
+}
+
+void InitVBO(RenderUnit& r) {
+    if (r.VertexData != nullptr) {
         glGenBuffers(1, &r.VBO);
         glBindBuffer(GL_ARRAY_BUFFER, r.VBO);
-        glBufferData(GL_ARRAY_BUFFER, (r.DataSize*sizeof(float)), r.VertexData.get(), GL_STATIC_DRAW);
+        glBufferData(GL_ARRAY_BUFFER, (r.VertexDataSize * sizeof(float)), r.VertexData, GL_STATIC_DRAW);
+        std::cout<<"DEBUG vbo size"<<r.VertexDataSize * sizeof(float) << std::endl;
     }
 }
 
-void InitEBO(RenderUnit& r){
-    if(r.IndexData != nullptr){
+void InitEBO(RenderUnit& r) {
+    if (r.IndexData != nullptr) {
         glGenBuffers(1, &r.EBO);
-        glBindBuffer(GL_ARRAY_BUFFER, r.EBO);
-        glBufferData(GL_ARRAY_BUFFER, (r.DataSize*sizeof(float)), r.IndexData.get(), GL_STATIC_DRAW);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, r.EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, (r.IndexDataSize * sizeof(unsigned int)), r.IndexData, GL_STATIC_DRAW);
+        std::cout<<"DEBUG ebo size"<<r.IndexDataSize* sizeof(unsigned int) << std::endl;
     }
 }
 
-void InitVAO(RenderUnit& r){
+void InitVAO(RenderUnit& r) {
     glGenVertexArrays(1, &r.VAO);
     glBindVertexArray(r.VAO);
-    glVertexAttribPointer(0, r.Stride, GL_FLOAT, GL_FALSE, r.Stride*sizeof(float), (void*)0);
+    glVertexAttribPointer(0, r.Stride, GL_FLOAT, GL_FALSE, r.Stride * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 }
 
+};  // namespace RenderUnit
 };  // namespace CowRender
